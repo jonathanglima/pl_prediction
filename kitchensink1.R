17dfb6513706895cb42331cb0466ad1667bae8e5
Jonathan Garcia Lima <jonathanglima@gmail.com>
2016-10-22 05:10:39 -0200

    adding training data

--- kitchensink1.R
+++ kitchensink1.R
@@ -15,7 +15,7 @@ legis <- fromJSON(file="camara_sp_downloads/data/legis.json")
 #                                                  id = "id1")
 
 ldply(legis, function(x) {
-  if ("url_pdf" %in% names(x) & tolower(x$tipo) == "pl") {
+  if ("url_pdf" %in% names(x) & tolower(x$tipo) == "pl" & "encerramento" %in% names(x)) {
     tmp_path <- paste0("tmp/", x$`_id`, ".pdf")
     if (!file.exists(tmp_path)) {
       download.file(x$url_pdf, destfile = tmp_path)
@@ -23,20 +23,12 @@ ldply(legis, function(x) {
   }
 }, .parallel=TRUE)
 
-ldply(head(legis), function(x) {
-  if ("url_pdf" %in% names(x) & tolower(x$tipo) == "pl") {
-    tmp_path <- paste0("tmp/", x$`_id`, ".pdf")
-    download.file(x$url_pdf, destfile = tmp_path)
-  }
-})
-
-adply(head(pdf_files), 1, function(x) {
-  fonts <- system(paste0("pdffonts ", x), intern = TRUE)
-  data.frame(is_text=NROW(fonts) > 2)
-})
+autores2012 <- read.csv(file="camara_sp_downloads/autores.csv", header = FALSE)
 
 df_legis <- ldply(legis, function(x) {
   if ("url_pdf" %in% names(x) & tolower(x$tipo) == "pl" & "encerramento" %in% names(x)) {
+    autores2012 <- read.csv(file="camara_sp_downloads/autores.csv", header = FALSE)
+    
     file <- paste0("tmp/", x$`_id`, ".pdf")
     fonts <- system(paste0("pdffonts ", file), intern = TRUE)
     is_text <- NROW(fonts) > 2
@@ -48,6 +40,7 @@ df_legis <- ldply(legis, function(x) {
                  autores=paste(x$autores, collapse = ' '),
                  ementa=paste(x$ementa, collapse = ' '),
                  ano=x$ano,
+                 qtd_votos=sum(autores2012[autores2012$V1 %in% x$autores, "V2"]),
                  encerramento=x$encerramento
                  )
 #       TODO: apply OCR below
@@ -152,38 +145,44 @@ ementa_corpus <- tm_map(ementa_corpus, removeWords, stopwords("portuguese"))
 ementa_corpus <- tm_map(ementa_corpus, removePunctuation)
 ementa_corpus <- tm_map(ementa_corpus, stripWhitespace)
 
-ementa_dtm <- DocumentTermMatrix(assuntos_corpus, control = list(bounds = list(global = c(0, Inf)))) 
+ementa_dtm <- DocumentTermMatrix(ementa_corpus, control = list(bounds = list(global = c(0, Inf)))) 
 dim(ementa_dtm)
 ementa_matrix <- as.matrix(ementa_dtm)
 colnames(ementa_matrix) <- paste("ementa", colnames(ementa_matrix), sep = "_")
 
 df_legis_final <- cbind(df_legis, as.data.frame(text_matrix), as.data.frame(assuntos_matrix), as.data.frame(autores_matrix), as.data.frame(ementa_matrix), as.data.frame(comissoes_matrix))
 
-autores2012 <- read.csv(file="camara_sp_downloads/autores.csv", header = FALSE)
+df_legis_light <- cbind(df_legis, as.data.frame(assuntos_matrix), as.data.frame(comissoes_matrix))
+df_legis_light$encerramento <- NULL
 
 # text_corpus.copy <- text_corpus
 # text_corpus <- tm_map(text_corpus, stemCompletion, dictionary=text_corpus.copy)
 
-content(text_corpus[[1]])
+# content(text_corpus[[1]])
 
-inTraining <- createDataPartition(df_legis_final$promulgado, p = .85, list = FALSE)
-training <- df_legis_final[ inTraining,]
-testing  <- df_legis_final[-inTraining,]
+# inTraining <- createDataPartition(df_legis_final$promulgado, p = .75, list = FALSE)
+# training <- df_legis_final[ inTraining,]
+# testing  <- df_legis_final[-inTraining,]
 
-inTraining <- createDataPartition(training$promulgado, p = .85, list = FALSE)
-training <- df_legis_final[ inTraining,]
-validation  <- df_legis_final[-inTraining,]
+inTraining <- createDataPartition(df_legis_light$promulgado, p = .75, list = FALSE)
+training <- df_legis_light[ inTraining,]
+testing  <- df_legis_light[-inTraining,]
 
-localH2O <- h2o.init(ip = "localhost", port = 54321, startH2O = TRUE, max_mem_size = '1g', nthreads = -1)
+# inTraining <- createDataPartition(training$promulgado, p = .85, list = FALSE)
+# training <- df_legis_final[ inTraining,]
+# validation  <- df_legis_final[-inTraining,]
+
+localH2O <- h2o.init(ip = "localhost", port = 54321, startH2O = TRUE, max_mem_size = '4g', nthreads = -1)
 
 train_h2o <- as.h2o(training)
 test_h2o <- as.h2o(testing)
+# validate_h2o <- as.h2o(validation)
 
-trainingAndTestingGbmWithoutLabels50 <- h2o.gbm(x = setdiff(names(data), c("full_type")),
-                                                y = 'full_type',
+trainingAndTestingGbm50 <- h2o.gbm(x = setdiff(names(df_legis_light), c("promulgado")),
+                                                y = 'promulgado',
                                                 training_frame = train_h2o,
                                                 validation_frame = test_h2o,
-                                                model_id = "trainingAndTestingGbmWithoutLabels50",
+                                                model_id = "trainingAndTestingGbm50",
                                                 # distribution = 'multinomial',
                                                 ntrees = 50,
                                                 max_depth = 10,
@@ -192,5 +191,7 @@ trainingAndTestingGbmWithoutLabels50 <- h2o.gbm(x = setdiff(names(data), c("full
 h2o.auc(h2o.performance(trainingAndTestingGbmWithoutLabels50, test_h2o))
 
 h2o.saveModel(trainingAndTestingGbmWithoutLabels50, paste0(getwd(), "/analysis/kitchensink2/models"), force = TRUE)
+
+h2o.shutdown()
 
 stopCluster(cl)
